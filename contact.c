9946da687367fab7ce3b07c9be2824e0587e281a
JayanthBalan <jayanthsb.005@gmail.com>
2026-07-27 15:16:27 +0000

    major bug fix 2

--- contact.c
+++ contact.c
@@ -6,6 +6,8 @@
 #include "populate.h"
 #include <ctype.h>
 
+unsigned char index_name[MAX_CONTACTS], index_phone[MAX_CONTACTS], index_email[MAX_CONTACTS];
+
 void sortContactPopulate(AddressBook *addressBook) {
     // Populate function to add a contact to address book
     Contact *cont = &addressBook->contacts[addressBook->contactCount - 1];
--- contact.h
+++ contact.h
@@ -3,7 +3,7 @@
 
 #define MAX_CONTACTS 100
 
-unsigned char index_name[MAX_CONTACTS], index_phone[MAX_CONTACTS], index_email[MAX_CONTACTS];
+extern unsigned char index_name[MAX_CONTACTS], index_phone[MAX_CONTACTS], index_email[MAX_CONTACTS];
 
 typedef struct {
     char name[50];
